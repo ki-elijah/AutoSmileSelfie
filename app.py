1f40e92755ff0bdfe5d3b2ee17f244ad85f923c0
elabs <elijahknsubuga@gmail.com>
2021-12-29 14:50:05 +0300

    done

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ while True:
     for x, y, w, h in face:
         cv2.rectangle(frame, (x, y), (x, y, w, h), (0, 255, 255), 2)
         face_roi = frame[y:y+h, x:x+w]
+        gray_roi = gray[y:y+h, x:x+w]
+        smile = smile_cascade.detectMultiScale(gray_roi, 1.3, 25)
         
+        for x1, y1, w1, h1 in smile:
+            cv2.rectangle(face_roi, (x1, y1), (x1+w1, y1+h1), (0 , 0, 255), 2)
+            time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+            file_name = f'selfie-{time_stamp}.png'
+            cv2.imwrite(file_name, original_frame)
+    
+    cv2.imshow('cam star', frame)
+    
+    if cv2.waitKey(10) == ord('q'):
+        break
         
